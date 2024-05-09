e40f40e1dc72125347be4ee061a66fd931b44ee8
Imran <imran.mia@portonics.com>
2024-05-09 13:16:01 +0600

    init

---
+++ ExpenseAITracker/ExpenseAITrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseAITrackerApp.swift
+//  ExpenseAITracker
+//
+//  Created by Imran on 9/5/24.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseAITrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
